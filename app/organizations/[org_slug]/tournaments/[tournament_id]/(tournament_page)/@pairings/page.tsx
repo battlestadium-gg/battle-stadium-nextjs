c3e13e6b54af9275497be7ffb8c31078b5b97e44
thatguyinabeanie <thatguyin@beanie.gg>
2024-10-30 10:50:19 -0500

    copy code over from previous repo

---
+++ app/organizations/[org_slug]/tournaments/[tournament_id]/(tournament_page)/@pairings/page.tsx
@@ -0,0 +1,14 @@
+import { Card, CardBody, CardHeader } from "~/components/nextui/client-components";
+
+const Pairings = () => {
+  return (
+    <Card>
+      <CardHeader>Pairings</CardHeader>
+      <CardBody>
+        <p>Pairings content</p>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default Pairings;
